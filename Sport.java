d83b51c23b0887a1dd41e9f6b3ba842f1ad18657
Cleamlnp199 <98035758+Cleamlnp199@users.noreply.github.com>
2022-02-23 17:30:44 +0100

    Constructeurs + Get Set Sport

--- Sport.java
+++ Sport.java
@@ -1,58 +1,32 @@
 package RegiSport;
 
-public class Pratiquer
+public class Sport
 {
-	private int Id_Equipe;
 	private int Id_Sport;
-	private int Id_Personne;
+	private String Libelle_S;
 	
-	
-	//Constructeur Pratiquer
-	public Pratiquer(int id_Equipe, int id_Sport, int id_Personne)
+	public Sport(int id_Sport, String libelle_S)
 	{
-		this.Id_Equipe = id_Equipe;
 		this.Id_Sport = id_Sport;
-		this.Id_Personne = id_Personne;
-	}
-	
-	
-	//Constructeur vide Pratiquer
-	public Pratiquer()
-	{
-		this.Id_Equipe = 0;
-		this.Id_Sport = 0;
-		this.Id_Personne = 0;
+		this.Libelle_S = libelle_S;
 	}
 
-
+	
 	//Génération des Get et Set
-	public int getId_Equipe() {
-		return Id_Equipe;
-	}
-
-
-	public void setId_Equipe(int id_Equipe) {
-		Id_Equipe = id_Equipe;
-	}
-
-
 	public int getId_Sport() {
 		return Id_Sport;
 	}
-
 
 	public void setId_Sport(int id_Sport) {
 		Id_Sport = id_Sport;
 	}
 
-
-	public int getId_Personne() {
-		return Id_Personne;
+	public String getLibelle_S() {
+		return Libelle_S;
 	}
 
-
-	public void setId_Personne(int id_Personne) {
-		Id_Personne = id_Personne;
+	public void setLibelle_S(String libelle_S) {
+		Libelle_S = libelle_S;
 	}
 	
 }
