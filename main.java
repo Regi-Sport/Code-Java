f9bbe863201a7748288422b6b30b78bb75380741
Cleamlnp199 <98035758+Cleamlnp199@users.noreply.github.com>
2022-03-09 17:40:21 +0100

    Update
    17h40 le 2022-03-09

--- main.java
+++ main.java
@@ -1,14 +1,14 @@
-package RegiSport;
-
 import java.sql.*;
 
-public class main {
-
+public class main
+{
+	
 	public static void main(String[] args)
 	{
 		
-		//ACCES A NOTRE BASE DE DONNEES
-		String url= "jdbc:mysql://127.0.0.1:3306/regi'sport";
+		// Préparation et Accès à la base de données
+		
+		String url= "jdbc:mysql://127.0.0.1:3306/regisport";
 		String login = "root";
 		String passwd="";
 		java.sql.Connection cn = null;
@@ -17,27 +17,55 @@ public class main {
 		
 		try
 		{
-			//Etape 1 : Chargement du driver
+			//Chargement du Driver
 			Class.forName("com.mysql.jdbc.Driver");
 			
-			
-			//Etape 2 : rÃ©cupÃ©ration de la connexion
+			//Connexion
 			cn = DriverManager.getConnection(url,login,passwd);
-			//DriverManager.getConnection(url,login,passwd);
+			/*DriverManager.getConnection(url,login,passwd);*/
 			
-			
-			//Etape 3: CrÃ©ation d'un statement
+			//Statement
 			st = cn.createStatement();
 			
 			
-			//Etape 5 : Execution RequÃªte Avec retour rÃ©sultat
-			String sql2 = "insert into sport(id_sport,libelle_s) values(1,'azerty');";
-			st.executeUpdate(sql2);
 			
+			
+			// Requête : 
+			
+			/* Compétitions à Venir */
+			String sql = "SELECT * FROM `competition` WHERE DATE_DEBUT > (SELECT date(now()))";
+			
+			/* Compétitions en Cours */
+			String sql2 = "SELECT * FROM `competition` WHERE DATE_DEBUT < (SELECT date(now())) AND (SELECT date(now())) < DATE_FIN";
+			
+			/* Compétitions Terminés */
+			String sql3 = "SELECT * FROM `competition` WHERE DATE_FIN < (SELECT date(now()))";
+			
+			/* Modifier Compétitions */
+			String id = ""/*textField.getText()*/;
+			String sql4 = "SELECT NOM FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql5 = "SELECT ID_SPORT FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql6 = "SELECT DATE_DEBUT FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql7 = "SELECT DATE_FIN FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql8 = "SELECT PRIX FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql9 = "SELECT VAINQUEUR FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql10 = "SELECT PARTICIPANTS FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql11 = "SELECT LIEU FROM `competition` WHERE ID_COMPETITION="+id;
+			String sql12 = "SELECT PAYS FROM `competition` WHERE ID_COMPETITION="+id;
+			
+			
+			
+			
+			/*rs = st.executeQuery(sql);
+			while (rs.next())
+			{
+				int id = rs.getInt("ID_SPORT");
+				String libelle = rs.getString("LIBELLE_S");
+			}*/
 		}
 		catch (SQLException e)
 		{
-			e.printStackTrace();
+		e.printStackTrace();
 		}
 		catch (ClassNotFoundException e)
 		{
