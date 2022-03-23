f447920c6b971f4cb75729081954cfcbff591dd6
Cleamlnp199 <98035758+Cleamlnp199@users.noreply.github.com>
2022-03-23 17:57:23 +0100

    Update

--- Competitions.java
+++ Competitions.java
@@ -7,7 +7,7 @@ public class Competitions
 	private String Libelle_C;
 	private String Date_Debut;
 	private String Date_Fin;
-	private double Recompense;
+	private String Recompense;
 	private String Vainqueur;
 	private int Nb_Participants;
 	private String Lieu;
@@ -15,7 +15,7 @@ public class Competitions
 	
 	//Constructeur de la competition
 	public Competitions(int id_Competition, int id_Sport, String libelle_C,
-			String date_Debut, String date_Fin, double recompense, String vainqueur,
+			String date_Debut, String date_Fin, String recompense, String vainqueur,
 			int nb_Participants, String lieu)
 	{
 		this.Id_Competition = id_Competition;
@@ -38,10 +38,26 @@ public class Competitions
 		this.Libelle_C = "";
 		this.Date_Debut = "";
 		this.Date_Fin = "";
-		this.Recompense = 0;
+		this.Recompense = "";
 		this.Vainqueur = "";
 		this.Nb_Participants = 0;
 		this.Lieu = "";
+	}
+	
+	
+	// Méthode permettant d'afficher les informations d'une compétition //
+	
+	public void Afficher()
+	{
+		System.out.print(Id_Competition+"  ");
+		System.out.print(Id_Sport+"  ");
+		System.out.print(Libelle_C+"  ");
+		System.out.print(Date_Debut+"  ");
+		System.out.print(Date_Fin+"  ");
+		System.out.print(Recompense+"  ");
+		System.out.print(Vainqueur+"  ");
+		System.out.print(Nb_Participants+"  ");
+		System.out.println(Lieu);
 	}
 	
 	
@@ -49,56 +65,73 @@ public class Competitions
 	public int getId_Competition() {
 		return Id_Competition;
 	}
+	
 	public void setId_Competition(int id_Competition) {
 		Id_Competition = id_Competition;
 	}
+	
 	public int getId_Sport() {
 		return Id_Sport;
 	}
+	
 	public void setId_Sport(int id_Sport) {
 		Id_Sport = id_Sport;
 	}
+	
 	public String getLibelle_C() {
 		return Libelle_C;
 	}
+	
 	public void setLibelle_C(String libelle_C) {
 		Libelle_C = libelle_C;
 	}
+	
 	public String getDate_Debut() {
 		return Date_Debut;
 	}
+	
 	public void setDate_Debut(String date_Debut) {
 		Date_Debut = date_Debut;
 	}
+	
 	public String getDate_Fin() {
 		return Date_Fin;
 	}
+	
 	public void setDate_Fin(String date_Fin) {
 		Date_Fin = date_Fin;
 	}
-	public double getRecompense() {
+	
+	public String getRecompense() {
 		return Recompense;
 	}
-	public void setRecompense(int recompense) {
+	
+	public void setRecompense(String recompense) {
 		Recompense = recompense;
 	}
+	
 	public String getVainqueur() {
 		return Vainqueur;
 	}
+	
 	public void setVainqueur(String vainqueur) {
 		Vainqueur = vainqueur;
 	}
+	
 	public int getNb_Participants() {
 		return Nb_Participants;
 	}
+	
 	public void setNb_Participants(int nb_Participants) {
 		Nb_Participants = nb_Participants;
 	}
+	
 	public String getLieu() {
 		return Lieu;
 	}
+	
 	public void setLieu(String lieu) {
 		Lieu = lieu;
 	}
-
+	
 }
