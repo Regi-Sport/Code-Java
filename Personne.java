062f37282d6c80020ff60eacdc8ecc6ca2b4bbe0
Cleamlnp199 <98035758+Cleamlnp199@users.noreply.github.com>
2022-04-26 17:32:04 +0200

    Update

--- Personne.java
+++ Personne.java
@@ -1,9 +1,8 @@
 package RegiSport;
 
-public class Personne
+public class Personne extends Equipe
 {
 	private int Id_Personne;
-	private int Id_Equipe;
 	private String Nom_P;
 	private String Prenom;
 	private int Age;
@@ -11,11 +10,22 @@ public class Personne
 	
 	
 	//Constructeur Personne
-	public Personne(int id_Personne, int id_Equipe, String nom_P, String prenom,
+	public Personne(int id_Personne, int Id_Equipe, String nom_P, String prenom,
 			int age, String date_Naissance)
 	{
+		super(Id_Equipe);
 		this.Id_Personne = id_Personne;
-		this.Id_Equipe = id_Equipe;
+		this.Nom_P = nom_P;
+		this.Prenom = prenom;
+		this.Age = age;
+		this.Date_Naissance = date_Naissance;
+	}
+	
+	public Personne(int id_Personne, String Nom_E, String nom_P, String prenom,
+			int age, String date_Naissance)
+	{
+		super(Nom_E);
+		this.Id_Personne = id_Personne;
 		this.Nom_P = nom_P;
 		this.Prenom = prenom;
 		this.Age = age;
@@ -27,7 +37,6 @@ public class Personne
 	public Personne()
 	{
 		this.Id_Personne = 0;
-		this.Id_Equipe = 0;
 		this.Nom_P = "";
 		this.Prenom = "";
 		this.Age = 0;
@@ -40,7 +49,17 @@ public class Personne
 	public void Afficher()
 	{
 		System.out.print(Id_Personne+"  ");
-		System.out.print(Id_Equipe+"  ");
+		System.out.print(Nom_E+"  ");
+		System.out.print(Nom_P+"  ");
+		System.out.print(Prenom+"  ");
+		System.out.print(Age+"  ");
+		System.out.println(Date_Naissance);
+	}
+	
+	public void Afficher1()
+	{
+		System.out.print(Id_Personne+"  ");
+		System.out.print(Id_equipe+"  ");
 		System.out.print(Nom_P+"  ");
 		System.out.print(Prenom+"  ");
 		System.out.print(Age+"  ");
@@ -55,14 +74,6 @@ public class Personne
 
 	public void setId_Personne(int id_Personne) {
 		Id_Personne = id_Personne;
-	}
-
-	public int getId_Equipe() {
-		return Id_Equipe;
-	}
-
-	public void setId_Equipe(int id_Equipe) {
-		Id_Equipe = id_Equipe;
 	}
 
 	public String getNom_P() {
